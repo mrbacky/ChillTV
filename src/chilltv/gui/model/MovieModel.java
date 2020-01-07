904057481431df709235cfb60f162b590b3a8508
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2020-01-07 21:42:52 +0100

    displaying movie (not working)

--- src/chilltv/gui/model/MovieModel.java
+++ src/chilltv/gui/model/MovieModel.java
@@ -1,15 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package chilltv.gui.model;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
-public class MovieModel {
+import chilltv.be.Movie;
+import chilltv.bll.LogicFacade;
+import chilltv.bll.LogicManager;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
+public class MovieModel {
+    
+    private LogicFacade logicManager;
+    private ObservableList<Movie> libraryList;
+
+    public MovieModel() {
+        logicManager = new LogicManager();
+        getAllMovies();
+    }
+    
+    public ObservableList<Movie> getAllMovies(){
+        List<Movie> allMovies = logicManager.getAllMovies();
+        libraryList = FXCollections.observableArrayList(allMovies);
+        return libraryList;
+    }
+    
+    
 }
