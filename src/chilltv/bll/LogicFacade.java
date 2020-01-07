0aa041cd94a45e877490ebbf8ad99aa3db334b00
anneluong <54981148+anneluong@users.noreply.github.com>
2020-01-07 10:29:27 +0100

    Search & Category related methods
    Added methods in the classes:
    SearchFilter
    DBFacade
    LogicFacade
    LogicManager

--- src/chilltv/bll/LogicFacade.java
+++ src/chilltv/bll/LogicFacade.java
@@ -1,6 +1,43 @@
+package chilltv.bll;
 
-package appletv.bll;
+import chilltv.be.Category;
+import java.util.List;
 
+/**
+ * This interface is a facade for the business logic layer.
+ *
+ * @author annem
+ */
 public interface LogicFacade {
-    
+
+    /**
+     * Creates and adds a new category to the database.
+     *
+     * @param name The name of the category.
+     */
+    void createCategory(String name);
+
+    /**
+     * Gets a list of the names of all the categories from the database,
+     *
+     * @return A list of all the categories.
+     */
+    List<String> getAllCategories();
+
+    /**
+     * Updates the name of a category in the database.
+     *
+     * @param category The category to be updated.
+     * @param editedName The edited name of the category.
+     * @return The updated category.
+     */
+    Category updateCategory(Category category, String editedName);
+
+    /**
+     * Deletes a category in the database.
+     *
+     * @param name The name of the category.
+     */
+    void deleteCategory(String name);
+
 }
--- src/chilltv/bll/util/SearchFilter.java
+++ src/chilltv/bll/util/SearchFilter.java
@@ -1,6 +1,8 @@
 package chilltv.bll.util;
 
-
+import chilltv.be.Movie;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The SearchFilter class is a tool used to filter out song items, which match
@@ -10,5 +12,26 @@ package chilltv.bll.util;
  */
 public class SearchFilter {
 
-    
+    /**
+     * Filters a list of movies and returns a filtered list matching the search
+     * query.
+     *
+     * @param searchBase The list of movies to filter.
+     * @param query The search query.
+     * @return A list of movies that matches the search query.
+     */
+    public List<Movie> search(List<Movie> searchBase, String query) {
+        //case insensitive and partial search
+        List<Movie> filtered = new ArrayList();
+
+        if (query.isEmpty()) {
+            return searchBase;
+        }
+        for (Movie movie : searchBase) {
+            if (movie.getTitle().toLowerCase().contains(query.trim().toLowerCase())) {
+                filtered.add(movie);
+            }
+        }
+        return filtered;
+    }
 }
