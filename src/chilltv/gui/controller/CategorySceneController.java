f4fb47346baf946c44770e85562086169aa157ed
anneluong <54981148+anneluong@users.noreply.github.com>
2020-01-17 11:40:23 +0100

    CategorySceneController + CategoryScene
    java documentation and rearranging order of methods.

--- src/chilltv/gui/controller/CategorySceneController.java
+++ src/chilltv/gui/controller/CategorySceneController.java
@@ -16,52 +16,75 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+/**
+ * The CategorySceneController is the controller for the CategoryScene. It sends
+ * requests to the Category Model, when creating, updating and deleting a
+ * category.
+ *
+ * @author annem
+ */
 public class CategorySceneController implements Initializable {
 
     @FXML
-    private Label lbl_Categories;
-    @FXML
-    private TextField txt_createCategory;
+    private Label lbl_categories;
     @FXML
     private Label lbl_newCat;
     @FXML
-    private Button btn_createCat;
-    @FXML
-    private Button btn_cancel;
-    private LibraryController libCon;
-    @FXML
-    private Button btn_addCategoryVisible;
-    @FXML
-    private Button btn_deleteCat;
-    private boolean edit;
-    private CategoryModel catModel;
-    private LibraryController libraryController;
-    private Category selectedCategory;
-    @FXML
     private ChoiceBox<Category> choiceBox_category;
     @FXML
+    private Button btn_createCategoryVisible;
+    @FXML
+    private Button btn_createCat;
+    @FXML
     private Button btn_editCat;
+    @FXML
+    private Button btn_deleteCat;
+    @FXML
+    private Button btn_cancel;
+    @FXML
+    private TextField txt_createCategory;
 
+    private LibraryController libraryController;
+    private CategoryModel catModel;
+    private boolean edit;
+    private Category selectedCategory;
+
+    /**
+     * Initializes the controller class. Upon initialization, the mode is set to
+     * create a category. The CategoryModel is initialized. All categories are
+     * added to the ChoiceBox.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //  gets model
+        //Gets the current instance of the Category model.
         catModel = CategoryModel.getInstance();
 
-        //  gets categories into checkboxes
-        getAllCatsForCheckBox();
+        //Gets all existing categories into to ChoiceBox.
+        getAllCatsInChoiceBox();
 
         catModel.getInstance().getObsCategories().addListener(new InvalidationListener() {
             @Override
             public void invalidated(Observable observable) {
                 //catModel.loadAllCategories();
-                getAllCatsForCheckBox();
+                getAllCatsInChoiceBox();
                 //setCheckedCategoriesForMovie();
             }
         });
-
     }
 
-    private void getAllCatsForCheckBox() {
+    /**
+     * Sets the controller for the LibraryScene
+     *
+     * @param libraryController LibraryController.
+     */
+    public void setContr(LibraryController libraryController) {
+        this.libraryController = libraryController;
+    }
+
+    /**
+     * Gets all the existing categories into the ChoiceBox.
+     */
+    private void getAllCatsInChoiceBox() {
         choiceBox_category.getItems().clear();
         ObservableList<Category> allCats = catModel.getObsCategories();
         for (Category cat : allCats) {
@@ -69,9 +92,58 @@ public class CategorySceneController implements Initializable {
         }
     }
 
+    /**
+     * Makes the the controls visible, when creating/editing a category.
+     */
+    private void setVisible() {
+        btn_createCat.setVisible(true);
+        lbl_newCat.setVisible(true);
+        txt_createCategory.setVisible(true);
+    }
+
+    /**
+     * Makes the the controls visible, when creating a category.
+     */
+    @FXML
+    private void handle_createCategoryVisible(ActionEvent event) {
+        setVisible();
+    }
+
+    /**
+     * Makes the the controls visible, when editing a category.
+     */
+    @FXML
+    private void handle_editCatVisible(ActionEvent event) {
+        setVisible();
+        selectedCategory = choiceBox_category.getSelectionModel().getSelectedItem();
+        editMode(selectedCategory);
+    }
+
+    /**
+     * Hides the controls after new/edit.
+     */
+    private void hideControls() {
+        btn_createCat.setVisible(false);
+        lbl_newCat.setVisible(false);
+        txt_createCategory.setVisible(false);
+    }
+
+    /**
+     * Enables the edit mode, so the category can be edited. The existing name
+     * of the selected category is displayed.
+     *
+     * @param selectedCategory
+     */
+    public void editMode(Category selectedCategory) {
+        edit = true;
+        txt_createCategory.setText(selectedCategory.getName());
+    }
+
+    /**
+     * Checks the selected mode (new or edit) and saves the category.
+     */
     @FXML
     public void handle_saveCategory(ActionEvent event) {
-
         if (!edit) {
             catModel.createCategory(txt_createCategory.getText().trim());
             txt_createCategory.setText("");
@@ -79,65 +151,26 @@ public class CategorySceneController implements Initializable {
         } else {
             selectedCategory.setName(txt_createCategory.getText().trim());
             catModel.updateCategory(selectedCategory);
-
         }
 
         hideControls();
-        
     }
 
-    
-
-
-    @FXML
-    private void handle_cancelScene(ActionEvent event) {
-        Stage stage = (Stage) btn_cancel.getScene().getWindow();
-        stage.close();
-        
-    }
-
+    /**
+     * Deletes the selected the category.
+     */
     @FXML
     private void handle_deleteCategory(ActionEvent event) {
         Category catToDelete = choiceBox_category.getSelectionModel().getSelectedItem();
         catModel.deleteCategory(catToDelete);
-
     }
 
+    /**
+     * Closes the stage.
+     */
     @FXML
-
-    private void handle_addCategoryVisible(ActionEvent event) {
-        setVisible();
+    private void handle_cancelScene(ActionEvent event) {
+        Stage stage = (Stage) btn_cancel.getScene().getWindow();
+        stage.close();
     }
-
-    private void setVisible() {
-
-        btn_createCat.setVisible(true);
-        lbl_newCat.setVisible(true);
-        txt_createCategory.setVisible(true);
-
-    }
-
-    private void hideControls() {
-        btn_createCat.setVisible(false);
-        lbl_newCat.setVisible(false);
-        txt_createCategory.setVisible(false);
-    }
-
-    void setContr(LibraryController libraryController) {
-        this.libraryController = libraryController;
-    }
-
-    @FXML
-    private void handle_editCatVisible(ActionEvent event) {
-        setVisible();
-        selectedCategory = choiceBox_category.getSelectionModel().getSelectedItem();
-        editMode(selectedCategory);
-        
-    }
-    
-    public void editMode(Category selectedCategory) {
-        edit = true;
-        txt_createCategory.setText(selectedCategory.getName());
-    }
-
 }
